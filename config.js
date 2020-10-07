52cce294f13b6f88f4af8bdf0c98f79848d9a60f
TheCtkHoster <69423422+TheCtkHoster@users.noreply.github.com>
2020-10-07 13:32:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-	"TOKEN": process.env.TOKEN,
-	"YOUTUBE_API_KEY": process.env.YOUTUBE_API_KEY,
+	"TOKEN": process.env.NzYzMjgyMzE1MDU2MjUwOTAw.X31cDQ.J8z_BAMyY3hhTpiZRFgxFQ8haJg,
+	"YOUTUBE_API_KEY": process.env.AIzaSyADuXzpEcS1omnZZgX_MKEXIlyepl5M6ss,
 	"MAX_PLAYLIST_SIZE": 10,
 	"PREFIX": "!",
 	"MUSIC_CHANNEL_ID": process.env.MUSIC_CHANNEL_ID
